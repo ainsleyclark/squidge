369aee964d46714ea0ba80db665486f62fff040d
Ainsley Clark <info@ainsleyclark.com>
2021-11-24 09:29:04 +0000

    Implemented service

--- inc/service.php
+++ inc/service.php
@@ -12,39 +12,131 @@
 
 namespace WPSquidge\Includes;
 
-abstract class WP_Squidge_Service {
+/**
+ * Jpeg Mime Type
+ */
+const JPEG_MIME = 'image/jpeg';
 
+/**
+ * PNG Mime Type
+ */
+const PNG_MIME = 'image/png';
+
+/**
+ * SVG Mime Type
+ */
+const SVG_MIME = 'image/svg+xml';
+
+/**
+ *
+ */
+abstract class WP_Squidge_Service
+{
     /**
      * The command name to run
      *
      * @var string
      */
-    protected $cmd_name = '';
+    protected string $cmd_name = '';
+
+    /**
+     * @var string
+     */
+    protected string $extension = '';
+
 
     /**
      * @param string $cmd_name
+     * @param string $extension
+     * @param false $should_delete
      */
-    public function __construct(string $cmd_name)
+    public function __construct(string $cmd_name, string $extension, $should_delete = false)
     {
         $this->cmd_name = $cmd_name;
-        add_action("add_attachment", [$this, 'convert']);
-        add_action("edit_attachment", [$this, 'convert']);
+        $this->extension = $extension;
+        add_filter("wp_generate_attachment_metadata", [$this, 'process'], 100, 1);
+        if ($should_delete) {
+            add_filter('delete_attachment', [$this, 'delete'], 100, 1);
+        }
     }
 
     /**
      * Converts or compresses an image.
      *
-     * @param array $images
      * @return mixed
      */
-    public abstract function convert(int $id);
+    public abstract function convert($filepath);
 
     /**
      *
      */
-    protected function get_images()
+    public function process($attachment)
     {
+        // Return if the cwebp library is not installed.
+        if (!$this->installed()) {
+            error_log("WP Squidge - cwebp is not installed.");
+            return $attachment;
+        }
 
+        // Check if the file key exists.
+        if (!isset($attachment['file'])) {
+            error_log("WP Squidge - file attachment is not set.");
+            return $attachment;
+        }
+
+        // Obtain the file and check if it exists.
+        $mainFile = $this->get_file_path($attachment['file']);
+        if (!$mainFile) {
+            error_log("WP Squidge - file does not exist");
+            return $attachment;
+        }
+
+        $mime = $this->get_mime_type($mainFile);
+        if (!$this->is_jpeg_or_png($mime)) {
+            return $attachment; // Mime cannot be converted, return.
+        }
+
+        // Convert main image.
+        $this->convert($mainFile);
+
+        // Loop over the sizes and convert them.
+        foreach ($attachment['sizes'] as $size) {
+            if (!isset($size['file'])) {
+                continue;
+            }
+            $path = $this->get_file_path($size['file']);
+            if (!$path) {
+                continue;
+            }
+            $this->convert($path);
+        }
+
+        return $attachment;
+    }
+
+    /**
+     * Deletes all webp images associated with the
+     * image (if the file exists).
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        $sizes = get_intermediate_image_sizes();
+
+        error_log(json_encode($sizes));
+
+        foreach ($sizes as $size) {
+            $src = wp_get_attachment_image_src($id, $size);
+            $path = $this->get_file_path($src[0] . $this->extension);
+            if (!$path) {
+                continue;
+            }
+            unlink($path);
+        }
+
+        return $id;
     }
 
     /**
@@ -57,4 +149,70 @@ abstract class WP_Squidge_Service {
         $return = shell_exec(sprintf("which %s", escapeshellarg($this->cmd_name)));
         return !empty($return);
     }
+
+    /**
+     * Determines if a mime type is jpeg or png.
+     *
+     * @param string $mime
+     * @return bool
+     */
+    private function is_jpeg_or_png(string $mime): bool
+    {
+        return ($mime === JPEG_MIME || $mime === PNG_MIME);
+    }
+
+    /**
+     * Obtains the absolute filepath including the
+     * upload directory.
+     * If the file does not exist on the file system, the
+     * function will return false.
+     *
+     * @param $path
+     * @return string
+     */
+    private function get_file_path($path)
+    {
+        $file = wp_get_upload_dir()['path'] . DIRECTORY_SEPARATOR . basename($path);
+        error_log("File" . $file);
+        if (file_exists($file)) {
+            return $file;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the mimetype of a file passed.
+     *
+     * @param $file
+     * @return false|string
+     */
+    private function get_mime_type($file)
+    {
+        return mime_content_type($file);
+    }
 }
+//
+//use WP_Query;
+//
+//add_action('init', function () {
+//
+//    $query_images_args = array(
+//        'post_type'      => 'attachment',
+//        'post_mime_type' => 'image',
+//        'post_status'    => 'inherit',
+//        'posts_per_page' => 5,
+//    );
+//
+//    $query_images = new WP_Query($query_images_args);
+//
+//    $images = array();
+//    foreach ($query_images->posts as $image) {
+//
+//        $webp = new WP_Squidge_WebP();
+//       //print_r($webp->)
+//
+//        echo '<pre>';
+//        print_r( $webp->get_attachment($image->ID));
+//    }
+//
+//});
--- inc/webp.php
+++ inc/webp.php
@@ -16,8 +16,9 @@
 
 namespace WPSquidge\Includes;
 
-use WP_Query;
-
+/**
+ *
+ */
 class WP_Squidge_WebP extends WP_Squidge_Service
 {
     /**
@@ -31,197 +32,23 @@ class WP_Squidge_WebP extends WP_Squidge_Service
     const EXTENSION = ".webp";
 
     /**
-     * The quality of the webp file.
-     *
-     * @var int
-     */
-    public $quality = 80;
-
-    /**
      * Sets up actions.
      */
     public function __construct()
     {
-        $this->quality = carbon_get_theme_option('wp_squidge_webp_quality');
-        parent::__construct(self::CMD_NAME);
-        add_action('delete_attachment', [$this, 'delete']);
+        parent::__construct(self::CMD_NAME, self::EXTENSION, true);
     }
 
     /**
      * Converts all image sizes if they are jpg or png
      * to webp with the given file extension.
      *
-     * @param array $images
+     * @param $path
      * @return void
      */
-    public function convert(int $id)
+    public function convert($path)
     {
-        if (!$this->installed()) {
-            error_log("WP Squidge - cwebp is not installed");
-            return;
-        }
-
-
-        error_log($id);
-
-        //        foreach ($images as $image) {
-        //            exec("cwebp -q " . $this->quality . " " . $image['path'] . " -o " . $image['path'] . self::EXTENSION);
-        //        }
-    }
-
-    /**
-     * Deletes all webp images associated with the
-     * image (if the file exists).
-     *
-     * @param int $id
-     */
-    public function delete(int $id)
-    {
-        $images = get_attached_file($id); // Gets path to attachment
-
-        foreach ($images as $image) {
-            if (file_exists($image['path'] . self::EXTENSION)) {
-                unlink($image['path']);
-            }
-        }
-    }
-
-    /**
-     * Returns an array of attachment data.
-     *
-     * @date    05/01/2015
-     * @param int|WP_Post The attachment ID or object.
-     * @return  array|false
-     * @since   5.1.5
-     *
-     */
-    protected function get_attachment($attachment)
-    {
-        // Get the attachment post object.
-        $attachment = get_post($attachment);
-        if (!$attachment) {
-            return false;
-        }
-        if ($attachment->post_type !== 'attachment') {
-            return false;
-        }
+        $quality = carbon_get_theme_option('wp_squidge_webp_quality');
+        exec("cwebp -q " . $quality . " " . $path . " -o " . $path . self::EXTENSION);
     }
 }
-
-
-/**
- * Returns an array of attachment data.
- *
- * @date    05/01/2015
- * @param int|WP_Post The attachment ID or object.
- * @return    array|false
- * @since    5.1.5
- *
- */
-function wp_squidge_get_attachment($attachment)
-{
-    // Get the attachment post object.
-    $attachment = get_post($attachment);
-    if (!$attachment) {
-        return false;
-    }
-    if ($attachment->post_type !== 'attachment') {
-        return false;
-    }
-
-    // Load various attachment details.
-    $meta = wp_get_attachment_metadata($attachment->ID);
-    $attached_file = get_attached_file($attachment->ID);
-    if (strpos($attachment->post_mime_type, '/') !== false) {
-        list($type, $subtype) = explode('/', $attachment->post_mime_type);
-    } else {
-        list($type, $subtype) = array($attachment->post_mime_type, '');
-    }
-
-    // Generate response.
-    $response = array(
-        'ID'          => $attachment->ID,
-        'title'       => $attachment->post_title,
-        'filepath'    => get_attached_file($attachment->ID),
-        'filename'    => wp_basename($attached_file),
-        'filesize'    => 0,
-        'url'         => wp_get_attachment_url($attachment->ID),
-        'author'      => $attachment->post_author,
-        'description' => $attachment->post_content,
-        'caption'     => $attachment->post_excerpt,
-        'name'        => $attachment->post_name,
-        'status'      => $attachment->post_status,
-        'uploaded_to' => $attachment->post_parent,
-        'date'        => $attachment->post_date_gmt,
-        'mime_type'   => $attachment->post_mime_type,
-        'type'        => $type,
-        'subtype'     => $subtype,
-        'icon'        => wp_mime_type_icon($attachment->ID)
-    );
-
-    // Append filesize data.
-    if (isset($meta['filesize'])) {
-        $response['filesize'] = $meta['filesize'];
-    } elseif (file_exists($attached_file)) {
-        $response['filesize'] = filesize($attached_file);
-    }
-
-    // Restrict the loading of image "sizes".
-    $sizes_id = 0;
-
-    if ($type === 'image') {
-        $sizes_id = $attachment->ID;
-        $src = wp_get_attachment_image_src($attachment->ID, 'full');
-        if ($src) {
-            $response['url'] = $src[0];
-            $response['width'] = $src[1];
-            $response['height'] = $src[2];
-        }
-    }
-
-    // Load array of image sizes.
-    if( $sizes_id ) {
-        $sizes = get_intermediate_image_sizes();
-        $sizes_data = array();
-        foreach( $sizes as $size ) {
-            $src = wp_get_attachment_image_src( $sizes_id, $size );
-
-            //print_r(image_get_intermediate_size($attachment->ID, $size));
-
-           // print_r(wp_upload_dir());
-
-            // https://stackoverflow.com/questions/36700286/is-there-any-away-to-get-absolute-path-of-product-image-featured-image-in-wordpr
-
-            if ( $src ) {
-                $sizes_data[ $size ] = $src[0];
-                $sizes_data[ $size . '-width' ] = $src[1];
-                $sizes_data[ $size . '-height' ] = $src[2];
-            }
-        }
-        $response['sizes'] = $sizes_data;
-    }
-
-
-    return $response;
-}
-
-add_action('init', function () {
-    $query_images_args = array(
-        'post_type'      => 'attachment',
-        'post_mime_type' => 'image',
-        'post_status'    => 'inherit',
-        'posts_per_page' => -1,
-    );
-
-    $query_images = new WP_Query($query_images_args);
-
-    $images = array();
-    foreach ($query_images->posts as $image) {
-
-
-
-        echo '<pre>';
-        print_r(wp_squidge_get_attachment($image->ID));
-    }
-
-});
